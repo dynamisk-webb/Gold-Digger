b6ce96a6b194f79d97849fd5a0fdc2b55400a22f
Julia Wang <julia.v.wang@gmail.com>
2023-04-06 14:44:53 +0200

    Add routes

--- gold-digger/src/App.js
+++ gold-digger/src/App.js
@@ -2,13 +2,18 @@
 import {BrowserRouter, Routes, Route} from "react-router-dom";
 import './static/App.css';
 import DiggerModel from "./DiggerModel.js";
-import Layout from "./views/layoutView.js";
 import Test from "./test/testPresenter.js";
-//import Home from "./views/homePresenter.js";
-//import Login from "./views/loginPresenter.js";
+import Artist from "./presenters/artistPresenter.js"
+import Home from "./presenters/homePresenter.js"
+import Loading from "./presenters/loadingPresenter.js"
+import Parameter from "./presenters/parameterPresenter.js"
+import Playlist from "./presenters/playlistPresenter.js"
+import Source from "./presenters/sourcePresenter.js"
+import Layout from "./views/Layout.js"
+import NoPage from "./views/NoPage.js"
 
 /**
- *  
+ *  Main App rendering all components
  */
 
 function App() {
@@ -18,14 +23,16 @@ function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" > {/* element={<Layout/>} */}
-          {/* Default route for / path 
+        <Route exact path="/loading" element={<Loading/>}/>
+        <Route path="/" element={<Layout/>}>
+          {/* Default route for / path */}
           <Route index element={<Home/>}/>
-          <Route path="login" element={<Login/>}/>
-          <Route path="*" element={<NoPage/>}/>          
-          */
-          }
+          <Route path="*" element={<NoPage/>}/>
           <Route path="test" element={<Test/>}/>
+          <Route path="artist" element={<Artist/>}/>
+          <Route path="parameter" element={<Parameter/>}/>
+          <Route path="playlist" element={<Playlist/>}/>
+          <Route path="source" element={<Source/>}/>
         </Route>
       </Routes>
     </BrowserRouter>
--- gold-digger/src/views/layoutView.js
+++ gold-digger/src/views/layoutView.js
@@ -1,7 +1,25 @@
-function LayoutView(){
-    return (<div>
-        
-    </div>);
-}
+import { Outlet, Link } from "react-router-dom";
+
+const LayoutView = () => {
+  return (
+    <>
+      <nav>
+        <ul>
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/blogs">Blogs</Link>
+          </li>
+          <li>
+            <Link to="/contact">Contact</Link>
+          </li>
+        </ul>
+      </nav>
+
+      <Outlet />
+    </>
+  )
+};
 
 export default LayoutView;
