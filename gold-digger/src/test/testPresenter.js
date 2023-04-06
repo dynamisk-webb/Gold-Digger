bd72e25ee326813804c73fcc2bbd1f483eec3d08
Julia Wang <julia.v.wang@gmail.com>
2023-04-06 13:44:47 +0200

    Update testPresenter.js comments

--- gold-digger/src/test/testPresenter.js
+++ gold-digger/src/test/testPresenter.js
@@ -1,8 +1,28 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import TestView from "./testView.js";
 
 function Test(props) {
+    // How it works:
+    // let sets is as a variable, and makes it changeable
+    // [saved variable (also called component state member), setter for the variable]
+    //
+    // useState is React's built-in function which defines a value to observe
+    // Only updates when the whole object changes (e.g. doesn't update when you adjust state.parameter)
     let [state, setState] = useState("Test");
+
+    // An example of a lifecycle function (probably)
+    function onMountACB() {
+        console.log("Lifecycle!");
+        setState("Hejdå");
+        // Add observer
+
+        // ACB to run at the end
+        return function onUnmountedACB() {
+            console.log("Cleanup!");
+            // Remove observer to keep it consistent
+        }
+    }
+    useEffect(onMountACB, []);
 
     return (
         <div>
@@ -10,7 +30,7 @@ function Test(props) {
         </div>
     )
 
-    
+    // Example of the state being updated
     function onChangeACB(value) {
         console.log("Example value sent: " + value.toString());
         setState(value);
