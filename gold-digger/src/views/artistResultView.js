6a6bb6cd07d61946e1c814e9c9a4cc48fdeb91f7
Fetaost-code <74512132+Fetaost-code@users.noreply.github.com>
2023-04-06 16:11:36 +0200

    Update artist and parameter view

--- gold-digger/src/views/artistResultView.js
+++ gold-digger/src/views/artistResultView.js
@@ -1,3 +1,6 @@
+import Radio from '@mui/material/Radio';
+import RadioGroup from '@mui/material/RadioGroup';
+
 function ArtistResultView(props){
     return (<div class="scrollable">
         {props.artistResults.map(getArtistACB)}
@@ -8,10 +11,26 @@ function ArtistResultView(props){
         return (
             <div> 
                 <p>result.title</p>
-                <button> Add </button>
-                <button> Remove </button>
+                <RadioGroup
+                aria-labelledby="demo-controlled-radio-buttons-group"
+                name="controlled-radio-buttons-group"
+                value={value}
+                onChange={handleChange}>
+                <FormControlLabel value="include" control={<Radio />} label="include" />
+                <FormControlLabel value="neutral" control={<Radio />} label="neutral" />
+                <FormControlLabel value="exclude" control={<Radio />} label="exclude" />
+                </RadioGroup>
             </div>
-        ) 
+        );
+    }
+
+    function handleChange(evt){
+        if(evt.target.value == "include")
+            props.setExcludeInclude(1);
+        if(evt.target.value == "neutral")
+            props.setExcludeInclude(0);
+        if(evt.target.value == "exclude")
+            props.setExcludeInclude(-1);
     }
 
     // TODO: create functions for pushing buttons
--- gold-digger/src/views/parameterView.js
+++ gold-digger/src/views/parameterView.js
@@ -6,33 +6,48 @@ function ParameterView(props){
         <div>
             <h2>Tempo</h2>
             <p>BPM</p>
-            <Slider getAriaLabel={() => 'Temperature range'} value={value} onChange={onChangeACB} valueLabelDisplay="auto" getAriaValueText={valuetext} />
+            <Slider getAriaLabel={() => 'Temperature range'} value={value} onChange={onChangeTempoACB} valueLabelDisplay="auto" getAriaValueText={valuetext} />
         </div>
         <div>
             <h2>Loudness</h2>
             <p>Db</p>
-            <Slider getAriaLabel={() => 'Temperature range'} value={value} onChange={onChangeACB} valueLabelDisplay="auto" getAriaValueText={valuetext} />
+            <Slider getAriaLabel={() => 'Temperature range'} value={value} onChange={onChangeLoudnessACB} valueLabelDisplay="auto" getAriaValueText={valuetext} />
         </div>
         <div>
             <h2>Instrumentalness</h2>
             <p>Percent</p>
-            <Slider getAriaLabel={() => 'Temperature range'} value={value} onChange={onChangeACB} valueLabelDisplay="auto" getAriaValueText={valuetext} />
+            <Slider getAriaLabel={() => 'Temperature range'} value={value} onChange={onChangeInstrumentalnessACB} valueLabelDisplay="auto" getAriaValueText={valuetext} />
         </div>
         <div>
             <h1>Danceable</h1>
-            <Switch checked={checked} onChange={onChangeACB}/>
+            <Switch checked={checked} onChange={onChangeDanceableACB}/>
         </div>
         <div>
             <h1>Acoustic</h1>
-            <Switch checked={checked} onChange={onChangeACB}/>
+            <Switch checked={checked} onChange={onChangeAcousticACB}/>
         </div>
         
     </div>);
 
-    function onChangeACB(){
-        // TODO: make it work uwu
+    function onChangeTempoACB(evt){
+        props.changeTempo(evt.target.value);
     }
-    
+
+    function onChangeLoudnessACB(evt){
+        props.changeLoudness(evt.target.value);
+    }
+
+    function onChangeInstrumentalnessACB(evt){
+        props.changeInstrumentalness(evt.target.value);
+    }
+
+    function onChangeDanceableACB(evt){
+        props.changeDanceable(evt.target.value);
+    }
+
+    function onChangeAcousticACB(){
+        props.changeAcoustic(evt.target.value);
+    }
 }
 
 export default ParameterView;
