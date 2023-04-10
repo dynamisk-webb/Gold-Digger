7d0f0ffd581f19fc2ce7886622bc2fdd11d01ce2
Julia <julia.v.wang@gmail.com>
2023-04-10 18:21:21 +0200

    Update artistPresenter.js

--- gold-digger/src/presenters/artistPresenter.js
+++ gold-digger/src/presenters/artistPresenter.js
@@ -3,15 +3,53 @@ import SearchView from "../views/searchView.js";
 import ArtistResultView from "../views/artistResultView.js";
 
 function Artists(props) {
+    const artist = [{
+          id: "4UXqAaa6dQYAk18Lv7PEgX",
+          name: "Fall Out Boy"
+        }, 
+        {
+          id: "0gxyHStUsqpMadRV0Di1Qt",
+          name: "Rick Astley"
+        },
+        {
+          id: "6goK4KMSdP4A8lw8jk4ADk",
+          name: "Pokémon"
+        },
+        {
+          id: "04gDigrS5kc9YWfZHwBETP",
+          name: "Maroon 5"
+        },
+        {
+          id: "137W8MRPWKqSmrBGDBFSop",
+          name: "Wiz Khalifa"
+        },
+        {
+          id: "5HZsYhRCMH3zR0yndRcLVw",
+          name: "MOB CHOIR"
+        },
+        {
+          id: "74XFHRwlV6OrjEM0A2NCMF",
+          name: "Paramore"
+        }
+    ];
 
-    // TODO: add parameter setIncludeExclude 
     return (
         <div>
             <FilterView filterType="artist" title="Include or Exclude Artists" nextTitle="Next"></FilterView>
             <SearchView></SearchView>
-            <ArtistResultView></ArtistResultView>
+            <ArtistResultView artistResults={artist} setExcludeInclude={setExcludeIncludeACB}></ArtistResultView>
         </div>
     );
+
+    function setExcludeIncludeACB(id, type) {
+        if(type === 1) {
+            props.model.includeArtist(id);
+        } else if(type === 0) {
+            props.model.removeArtist(id);
+        } else if(type === -1) {
+            props.model.excludeArtist(id);
+        }
+    }
 }
 
 export default Artists;
