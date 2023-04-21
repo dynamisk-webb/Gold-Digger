58904370cf5b80fe8a89c4e2db95e432bce5d429
gorwat <84105946+gorwat@users.noreply.github.com>
2023-04-21 21:46:30 +0200

    Update homePresenter.js

--- gold-digger/src/presenters/homePresenter.js
+++ gold-digger/src/presenters/homePresenter.js
@@ -11,38 +11,24 @@ function Home(props) {
     );
 
     /*
-
+    
+    TODO
     Lifecycle: GET prev playlists from Firebase
     For each previous playlist Event: onClick inspect playlist
 
     */
+    
     function onMountedACB() {
-        console.log("HOME access token: " + localStorage.getItem("access-token"));
-        console.log("HOME isLoggedIn (state and localstorage): " + props.model.isLoggedIn + ", " + localStorage.getItem("isLoggedIn"));
-
         if (localStorage.getItem("access-token") === null && props.model.isLoggedIn === "pending") {
             localStorage.setItem("access-token", "pending"); // prevents second request from comming through while running react strict mode
             props.model.requestToken().then(setLoggedInACB);
         }
-
-        // ACB to run at the end
-        return function onUnmountedACB() {
-        }
     }
 
-    // Set login 
     function setLoggedInACB() {
-        console.log("isLoggedIn before setting to true (state and localstorage)" + props.model.isLoggedIn + " " + localStorage.getItem("isLoggedIn"));
         props.model.setLogin("true");
         localStorage.setItem("isLoggedIn", "true");
-        console.log("isLoggedIn after setting to true (state and localstorage)" + props.model.isLoggedIn + " " + localStorage.getItem("isLoggedIn"));
     }    
-
-    /* Event: Set window location to Source */
-    function generatePlaylistACB() {
-        // go to next window, ie Source Presenter
-        return redirect("/source"); 
-    }
 }
 
 export default Home;
