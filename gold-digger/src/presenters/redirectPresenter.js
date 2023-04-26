a87bd7e8fc6e0fb26c6cc86ca53ea070ff5c82dd
Maria Moliteus <moliteus@kth.se>
2023-04-26 21:33:28 +0200

    redirect presenter

---
+++ gold-digger/src/presenters/redirectPresenter.js
@@ -0,0 +1,30 @@
+
+import { useEffect } from "react";
+
+function Redirect (props) {
+
+    useEffect(onMountedACB, [props.model]);
+
+    return (
+        <div></div>
+    );
+
+    /*
+    Is this one needed...?
+    
+    function setLoggedInACB() {
+        props.model.setLogin("true");
+        localStorage.setItem("isLoggedIn", "true");
+    }
+    */
+
+    function onMountedACB() {
+        if (localStorage.getItem("access-token") === null && props.model.isLoggedIn === "pending") {
+            localStorage.setItem("access-token", "pending"); // prevents second request from comming through while running react strict mode
+            props.model.requestToken().then(setLoggedInACB);
+        }
+    }
+
+}
+
+export default Redirect;
