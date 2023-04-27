64c9980699cbf984d5af04edbfb0b1911ad23490
Fetaost-code <74512132+Fetaost-code@users.noreply.github.com>
2023-04-27 15:34:02 +0200

    Update search view

--- gold-digger/src/views/searchView.js
+++ gold-digger/src/views/searchView.js
@@ -3,15 +3,19 @@ function SearchView(props){
     return (
         <div id="search">
             <img scr={magnify}></img>
-            <input type="text" onChange={changeInputACB}  placeholder="Search.." id="searchBar">
+                <input type="text" onKeyDown={changeInputACB}  placeholder="Search.." id="searchBar">
             </input>
         </div>
         );
     
-        //TODO: decide how to do search and implement
-    
         function changeInputACB(evt){
-            props.updateInput(evt.target.value);
+
+            let searchTerm = "";
+
+            if(evt.key === "Enter"){
+                searchTerm = document.getElementById("searchBar").value;
+                props.search(searchTerm);
+            }
         }
     
         function searchClickACB(evt){
