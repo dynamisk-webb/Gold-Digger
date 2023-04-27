f3d1e7620575791894c3011f81468cb464abf2bb
Maria Moliteus <moliteus@kth.se>
2023-04-27 13:00:04 +0200

    genrePresenter now compiles without errors

--- gold-digger/src/presenters/genrePresenter.js
+++ gold-digger/src/presenters/genrePresenter.js
@@ -4,26 +4,34 @@ import GenreResultView from "../views/genreResultView.js";
 import { getGenres } from "../spotifySource.js";
 import { useEffect, useState } from "react";
 import promiseNoData from "../views/promiseNoData.js";
+import resolvePromise from "../resolvePromise.js";
 
 function Genres(props) {
-    const [genres, setGenres] = useState();
+   
     const [promiseState, setState] = useState({});
     useEffect(()=>{    
         async function getGenreACB() {
-            let response = await getGenres();
-            setGenres(response);
-            console.log("Genres: " + genres[0]);
+            resolvePromise(getGenres(), promiseState, setState);
+            
+            // resolve promise with sort function, promiseState and setPromise
+            // resolvePromise(filterGenreACB, promiseState, setPromise);
         } 
         getGenreACB();
     }, []);
 
-    setGenres (["acoustic", "afrobeat", "alt-rock", "alternative", "ambient", "anime", "black-metal", "bluegrass", "blues", "bossanova", "brazil", "breakbeat", "british", "cantopop", "chicago-house", "children", "chill", "classical", "club", "comedy", "country", "dance", "dancehall", "death-metal", "deep-house", "detroit-techno", "disco", "disney", "drum-and-bass", "dub", "dubstep", "edm", "electro", "electronic", "emo", "folk", "forro", "french", "funk", "garage", "german", "gospel", "goth", "grindcore", "groove", "grunge", "guitar", "happy", "hard-rock", "hardcore", "hardstyle", "heavy-metal", "hip-hop", "holidays", "honky-tonk", "house", "idm", "indian", "indie", "indie-pop", "industrial", "iranian", "j-dance", "j-idol", "j-pop", "j-rock", "jazz", "k-pop", "kids", "latin", "latino", "malay", "mandopop", "metal", "metal-misc", "metalcore", "minimal-techno", "movies", "mpb", "new-age", "new-release", "opera", "pagode", "party", "philippines-opm", "piano", "pop", "pop-film", "post-dubstep", "power-pop", "progressive-house", "psych-rock", "punk", "punk-rock", "r-n-b", "rainy-day", "reggae", "reggaeton", "road-trip", "rock", "rock-n-roll", "rockabilly", "romance", "sad", "salsa", "samba", "sertanejo", "show-tunes", "singer-songwriter", "ska", "sleep", "songwriter", "soul", "soundtracks", "spanish", "study", "summer", "swedish", "synth-pop", "tango", "techno", "trance", "trip-hop", "turkish", "work-out", "world-music"]);
+    // function getGenreACB() {}
+
+    // TODO: sort function. Sort the chosen genres to display. Sort based on... what?
+    function filterGenreACB() {
+        // filter out everything that matches the searchTerm from the list of genres
+
+    }
 
     // Add some loading
     return (
         <div id="genreMainGrid">
             <SearchView id="search"></SearchView>
-            {promiseNoData(promiseState) || <GenreResultView id="genreResults" genreResults={genres} setSelectDeselect={setSelectDeselectACB}></GenreResultView>}
+            {promiseNoData(promiseState) || <GenreResultView id="genreResults" genreResults={promiseState.data} setSelectDeselect={setSelectDeselectACB}></GenreResultView>}
             <FilterView filterType="genre" title="Select Genres" nextTitle="Next" ></FilterView>
         </div>
     );
