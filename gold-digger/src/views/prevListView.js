2a3770ddfdd76165dcb638cfee4643f518e756ca
Isak <rejimejlen@gmail.com>
2023-05-11 16:26:59 +0200

    Add text when prevplaylist empty #66

--- gold-digger/src/views/prevListView.js
+++ gold-digger/src/views/prevListView.js
@@ -6,9 +6,16 @@ function PrevlistView(props){
     return (<div id="prevListsContainer">
         <h3 id="prevListsTitle"> Previously generated playlists</h3>
         <div className="scrollable"  id="prevLists">
-        {props.prevPlaylists.map(songInfoACB)}
+        {renderPrevPlaylist()}
         </div>
     </div>);
+
+    function renderPrevPlaylist(){
+        if(props.prevPlaylists.length != 0){
+            return props.prevPlaylists.map(songInfoACB);
+        }
+        else return <p id="prevListText">Create some playlists to see them here!</p>
+    }
 
     function songInfoACB(playlist){
         function setCurrentPlaylistACB(){
