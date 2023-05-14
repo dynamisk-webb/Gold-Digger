43db15557e98148a99c3374d8c449391ac6ff358
Julia <julia.v.wang@gmail.com>
2023-05-14 16:44:17 +0200

    Update sourcePresenter.js

--- gold-digger/src/presenters/sourcePresenter.js
+++ gold-digger/src/presenters/sourcePresenter.js
@@ -1,5 +1,5 @@
 import SourceView from "../views/sourceView";
-import { getPlaylistID } from "../spotifySource";
+import { getPlaylistInfo } from "../spotifySource";
 import { useState, useEffect } from "react";
 
 function Source(props) {
@@ -40,7 +40,7 @@ function Source(props) {
     
     if (url.startsWith("http://open.spotify.com/track/") || url.startsWith("https://open.spotify.com/playlist/")) {
       // Use API call 'getPlaylistID' to check if URL leads to an exisiting playlist
-      getPlaylistID(url).then(setValidSourceACB).catch(invalidSourceACB);
+      getPlaylistInfo(url).then(setValidSourceACB).catch(invalidSourceACB);
     } else {
       alert("Oops!\nThat doesn't look like a valid Spotify link.\n\nTo get a link, simply right-click any Spotify playlist,\nselect \"Share\" and then \"Copy link to playlist\"");
     }
